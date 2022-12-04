2a642516d682b9ee3dc85fb7d976e7f49592485c
MichelAguilera <48520019+MichelAguilera@users.noreply.github.com>
2022-12-04 12:35:11 +0100

    minor change V 0.0.1

--- src/shared/config.lua
+++ src/shared/config.lua
@@ -6,9 +6,9 @@ config = {}
 
 -- congig.Render
 config.Render = {
-    ["DataSize"] = 7;
-    ["RenderAmount"] = 7;
-    ["RenderFromPos"] = {x = 0, y = 3, z = 0};
+    ["DataSize"] = 0;
+    ["RenderAmount"] = 0; -- Keep equal or less than "DataSize"
+    ["RenderFromPos"] = {x = 0, y = 3, z = 0}; -- (y = 3) Places the clones on ground level
 }
 
 return config
